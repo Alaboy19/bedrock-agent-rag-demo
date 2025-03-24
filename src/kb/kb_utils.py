d3d70828f0a9a71a335199ef3a13b210248b5642
Nurbolat Bazarbek <47283347+Alaboy19@users.noreply.github.com>
2025-03-24 21:48:48 +0100

    Update kb_utils.py

--- src/kb/kb_utils.py
+++ src/kb/kb_utils.py
@@ -1,27 +1,9 @@
-import boto3
 import random
 import time
 import json
 
-suffix = random.randrange(200, 900)
-boto3_session = boto3.session.Session()
-region_name = boto3_session.region_name
-iam_client = boto3_session.client('iam')
-account_number = boto3.client('sts').get_caller_identity().get('Account')
-identity = boto3.client('sts').get_caller_identity()['Arn']
 
-encryption_policy_name = f"bedrock-sample-rag-sp-{suffix}"
-network_policy_name = f"bedrock-sample-rag-np-{suffix}"
-access_policy_name = f'bedrock-sample-rag-ap-{suffix}'
-bedrock_execution_role_name = f'AmazonBedrockExecutionRoleForKnowledgeBase_{suffix}'
-fm_policy_name = f'AmazonBedrockFoundationModelPolicyForKnowledgeBase_{suffix}'
-s3_policy_name = f'AmazonBedrockS3PolicyForKnowledgeBase_{suffix}'
-sm_policy_name = f'AmazonBedrockSecretPolicyForKnowledgeBase_{suffix}'
-oss_policy_name = f'AmazonBedrockOSSPolicyForKnowledgeBase_{suffix}'
-
-sm_policy_flag = False
-
-def create_bedrock_execution_role(bucket_name):
+def create_bedrock_execution_role(iam_client, bucket_name, account_number, suffix, region_name):
     foundation_model_policy_document = {
         "Version": "2012-10-17",
         "Statement": [
@@ -32,7 +14,8 @@ def create_bedrock_execution_role(bucket_name):
                 ],
                 "Resource": [
                     f"arn:aws:bedrock:{region_name}::foundation-model/amazon.titan-embed-text-v1",
-                    f"arn:aws:bedrock:{region_name}::foundation-model/amazon.titan-embed-text-v2:0"
+                    f"arn:aws:bedrock:{region_name}::foundation-model/amazon.titan-embed-text-v2:0",
+                    f"arn:aws:bedrock:{region_name}::foundation-model/cohere.embed-english-v3"
                 ]
             }
         ]
@@ -42,18 +25,36 @@ def create_bedrock_execution_role(bucket_name):
         "Version": "2012-10-17",
         "Statement": [
             {
+                "Sid": "S3ListBucketStatement",
                 "Effect": "Allow",
                 "Action": [
-                    "s3:GetObject",
                     "s3:ListBucket"
                 ],
                 "Resource": [
                     f"arn:aws:s3:::{bucket_name}",
+                ],
+                "Condition": {
+                    "StringEquals": {
+                        "aws:ResourceAccount": [
+                            f"{account_number}"
+                        ]
+                    }
+                }
+            },
+            {
+                "Sid": "S3GetObjectStatement",
+                "Effect": "Allow",
+                "Action": [
+                    "s3:GetObject"
+                ],
+                "Resource": [
                     f"arn:aws:s3:::{bucket_name}/*"
                 ],
                 "Condition": {
                     "StringEquals": {
-                        "aws:ResourceAccount": f"{account_number}"
+                        "aws:ResourceAccount": [
+                            f"{account_number}"
+                        ]
                     }
                 }
             }
@@ -74,19 +75,19 @@ def create_bedrock_execution_role(bucket_name):
     }
     # create policies based on the policy documents
     fm_policy = iam_client.create_policy(
-        PolicyName=fm_policy_name,
+        PolicyName= f'AmazonBedrockFoundationModelPolicyForKnowledgeBase_{suffix}',
         PolicyDocument=json.dumps(foundation_model_policy_document),
         Description='Policy for accessing foundation model',
     )
 
     s3_policy = iam_client.create_policy(
-        PolicyName=s3_policy_name,
+        PolicyName=f'AmazonBedrockS3PolicyForKnowledgeBase_{suffix}',
         PolicyDocument=json.dumps(s3_policy_document),
         Description='Policy for reading documents from s3')
 
     # create bedrock execution role
     bedrock_kb_execution_role = iam_client.create_role(
-        RoleName=bedrock_execution_role_name,
+        RoleName=f'AmazonBedrockExecutionRoleForKnowledgeBase_{suffix}',
         AssumeRolePolicyDocument=json.dumps(assume_role_policy_document),
         Description='Amazon Bedrock Knowledge Base Execution Role for accessing OSS and S3',
         MaxSessionDuration=3600
@@ -110,7 +111,7 @@ def create_bedrock_execution_role(bucket_name):
     return bedrock_kb_execution_role
 
 
-def create_oss_policy_attach_bedrock_execution_role(collection_id, bedrock_kb_execution_role):
+def create_oss_policy_attach_bedrock_execution_role(iam_client, collection_id, bedrock_kb_execution_role, suffix, account_number, region_name):
     # define oss policy document
     oss_policy_document = {
         "Version": "2012-10-17",
@@ -127,7 +128,7 @@ def create_oss_policy_attach_bedrock_execution_role(collection_id, bedrock_kb_ex
         ]
     }
     oss_policy = iam_client.create_policy(
-        PolicyName=oss_policy_name,
+        PolicyName=f'AmazonBedrockOSSPolicyForKnowledgeBase_{suffix}',
         PolicyDocument=json.dumps(oss_policy_document),
         Description='Policy for accessing opensearch serverless',
     )
@@ -141,9 +142,9 @@ def create_oss_policy_attach_bedrock_execution_role(collection_id, bedrock_kb_ex
     return None
 
 
-def create_policies_in_oss(vector_store_name, aoss_client, bedrock_kb_execution_role_arn):
+def create_policies_in_oss(vector_store_name, aoss_client, bedrock_kb_execution_role_arn, suffix, identity):
     encryption_policy = aoss_client.create_security_policy(
-        name=encryption_policy_name,
+        name=f"bedrock-sample-rag-sp-{suffix}",
         policy=json.dumps(
             {
                 'Rules': [{'Resource': ['collection/' + vector_store_name],
@@ -154,7 +155,7 @@ def create_policies_in_oss(vector_store_name, aoss_client, bedrock_kb_execution_
     )
 
     network_policy = aoss_client.create_security_policy(
-        name=network_policy_name,
+        name=f"bedrock-sample-rag-np-{suffix}",
         policy=json.dumps(
             [
                 {'Rules': [{'Resource': ['collection/' + vector_store_name],
@@ -164,7 +165,7 @@ def create_policies_in_oss(vector_store_name, aoss_client, bedrock_kb_execution_
         type='network'
     )
     access_policy = aoss_client.create_access_policy(
-        name=access_policy_name,
+        name=f'bedrock-sample-rag-ap-{suffix}',
         policy=json.dumps(
             [
                 {
@@ -197,39 +198,93 @@ def create_policies_in_oss(vector_store_name, aoss_client, bedrock_kb_execution_
     return encryption_policy, network_policy, access_policy
 
 
-def delete_iam_role_and_policies():
-    fm_policy_arn = f"arn:aws:iam::{account_number}:policy/{fm_policy_name}"
-    s3_policy_arn = f"arn:aws:iam::{account_number}:policy/{s3_policy_name}"
-    oss_policy_arn = f"arn:aws:iam::{account_number}:policy/{oss_policy_name}"
-    sm_policy_arn = f"arn:aws:iam::{account_number}:policy/{sm_policy_name}"
+# AWS Bedrock Knowledge Base
+def create_bedrock_knowledge_base(name, index_name, client, role_arn, collection_arn):
+    response = client.create_knowledge_base(
+        name=name,
+        roleArn=role_arn,
+        knowledgeBaseConfiguration={
+            "type": "VECTOR",
+            "vectorKnowledgeBaseConfiguration": {
+                "embeddingModelArn": "arn:aws:bedrock:us-east-1::foundation-model/amazon.titan-embed-text-v2:0",
+                "embeddingModelConfiguration": {
+                    "bedrockEmbeddingModelConfiguration": {
+                        "dimensions": 1024,
+                        "embeddingDataType": "FLOAT32"
+                    }
+                }
+            }
+        },
+        storageConfiguration={
+            "type": "OPENSEARCH_SERVERLESS",
+            "opensearchServerlessConfiguration": {
+                "collectionArn": collection_arn,
+                "vectorIndexName": f"{index_name}",
+                "fieldMapping": {
+                    "vectorField": "vector",
+                    "textField": "AMAZON_BEDROCK_TEXT_CHUNK",
+                    "metadataField": "AMAZON_BEDROCK_METADATA"
+                }
+            }
+        }
+    )
+    return response["knowledgeBase"]["knowledgeBaseArn"]
 
-    iam_client.detach_role_policy(
-        RoleName=bedrock_execution_role_name,
-        PolicyArn=s3_policy_arn
-    )
-    iam_client.detach_role_policy(
-        RoleName=bedrock_execution_role_name,
-        PolicyArn=fm_policy_arn
-    )
-    iam_client.detach_role_policy(
-        RoleName=bedrock_execution_role_name,
-        PolicyArn=oss_policy_arn
-    )
-
-    # Delete Secrets manager policy only if it was created (i.e. for Confluence, SharePoint or Salesforce data source)
-    if sm_policy_flag:
-        iam_client.detach_role_policy(
-            RoleName=bedrock_execution_role_name,
-            PolicyArn=sm_policy_arn
+def create_data_source_and_sync(client, kb_id, bucket_name):
+    response = client.create_data_source(
+        name="my-data-source",
+        knowledgeBaseId=kb_id,
+        dataSourceConfiguration={
+            'type': 'S3',
+            's3Configuration': {
+                'bucketArn': f'arn:aws:s3:::{bucket_name}'
+            }
+        },
+        vectorIngestionConfiguration={
+            'chunkingConfiguration': {
+                'chunkingStrategy': 'FIXED_SIZE',
+                'fixedSizeChunkingConfiguration': {
+                    'maxTokens': 500,
+                    'overlapPercentage': 10
+                }
+            }
+        },
+        description='My data source for knowledge base'
         )
-        iam_client.delete_policy(PolicyArn=sm_policy_arn)
-    iam_client.delete_role(RoleName=bedrock_execution_role_name)
-    iam_client.delete_policy(PolicyArn=s3_policy_arn)
-    iam_client.delete_policy(PolicyArn=fm_policy_arn)
-    iam_client.delete_policy(PolicyArn=oss_policy_arn)
-    
-    return 0
 
+    data_source_id = response['dataSource']['dataSourceId']
+    print(f"Data source created: {data_source_id}")
+
+    # Step 2: Sync the Data Source
+    sync_response = client.start_ingestion_job(
+        knowledgeBaseId=kb_id,
+        dataSourceId=data_source_id
+    )
+
+    ingestion_job_id = sync_response['ingestionJob']['ingestionJobId']
+    print("Ingestion job started:", ingestion_job_id)
+
+    # Poll for the ingestion job status
+    while True:
+        response = client.get_ingestion_job(
+            knowledgeBaseId=kb_id,
+            ingestionJobId=ingestion_job_id,
+            dataSourceId=data_source_id
+        )
+        status = response['ingestionJob']['status']
+        print(f"Ingestion job status: {status}")
+
+        if status in ['COMPLETE', 'FAILED']:
+            break
+
+        # Wait for a few seconds before polling again
+        time.sleep(10)
+
+    if status == 'COMPLETE':
+        print("Ingestion job completed successfully.")
+    elif status == 'FAILED':
+        print("Ingestion job failed.")
+        print("Error message:", response['ingestionJob'].get('errorMessage', 'No error message provided.'))
 
 def interactive_sleep(seconds: int):
     dots = ''
@@ -238,132 +293,3 @@ def interactive_sleep(seconds: int):
         print(dots, end='\r')
         time.sleep(1)
 
-def create_bedrock_execution_role_multi_ds(bucket_names = None, secrets_arns = None):
-    
-    # 0. Create bedrock execution role
-
-    assume_role_policy_document = {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Principal": {
-                    "Service": "bedrock.amazonaws.com"
-                },
-                "Action": "sts:AssumeRole"
-            }
-        ]
-    }
-    
-    # create bedrock execution role
-    bedrock_kb_execution_role = iam_client.create_role(
-        RoleName=bedrock_execution_role_name,
-        AssumeRolePolicyDocument=json.dumps(assume_role_policy_document),
-        Description='Amazon Bedrock Knowledge Base Execution Role for accessing OSS, secrets manager and S3',
-        MaxSessionDuration=3600
-    )
-
-    # fetch arn of the role created above
-    bedrock_kb_execution_role_arn = bedrock_kb_execution_role['Role']['Arn']
-
-    # 1. Cretae and attach policy for foundation models
-    foundation_model_policy_document = {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Action": [
-                    "bedrock:InvokeModel",
-                ],
-                "Resource": [
-                    f"arn:aws:bedrock:{region_name}::foundation-model/amazon.titan-embed-text-v1",
-                    f"arn:aws:bedrock:{region_name}::foundation-model/amazon.titan-embed-text-v2:0"
-                ]
-            }
-        ]
-    }
-    
-    fm_policy = iam_client.create_policy(
-        PolicyName=fm_policy_name,
-        PolicyDocument=json.dumps(foundation_model_policy_document),
-        Description='Policy for accessing foundation model',
-    )
-  
-    # fetch arn of this policy 
-    fm_policy_arn = fm_policy["Policy"]["Arn"]
-    
-    # attach this policy to Amazon Bedrock execution role
-    iam_client.attach_role_policy(
-        RoleName=bedrock_kb_execution_role["Role"]["RoleName"],
-        PolicyArn=fm_policy_arn
-    )
-
-    # 2. Cretae and attach policy for s3 bucket
-    if bucket_names:
-        s3_policy_document = {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Effect": "Allow",
-                    "Action": [
-                        "s3:GetObject",
-                        "s3:ListBucket"
-                    ],
-                    "Resource": [item for sublist in [[f'arn:aws:s3:::{bucket}', f'arn:aws:s3:::{bucket}/*'] for bucket in bucket_names] for item in sublist], 
-                    "Condition": {
-                        "StringEquals": {
-                            "aws:ResourceAccount": f"{account_number}"
-                        }
-                    }
-                }
-            ]
-        }
-        # create policies based on the policy documents
-        s3_policy = iam_client.create_policy(
-            PolicyName=s3_policy_name,
-            PolicyDocument=json.dumps(s3_policy_document),
-            Description='Policy for reading documents from s3')
-
-        # fetch arn of this policy 
-        s3_policy_arn = s3_policy["Policy"]["Arn"]
-        
-        # attach this policy to Amazon Bedrock execution role
-        iam_client.attach_role_policy(
-            RoleName=bedrock_kb_execution_role["Role"]["RoleName"],
-            PolicyArn=s3_policy_arn
-        )
-
-    # 3. Cretae and attach policy for secrets manager
-    if secrets_arns:
-        sm_policy_flag = True
-        secrets_manager_policy_document = {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Effect": "Allow",
-                    "Action": [
-                        "secretsmanager:GetSecretValue",
-                        "secretsmanager:PutSecretValue"
-                    ],
-                    "Resource": secrets_arns
-                }
-            ]
-        }
-        # create policies based on the policy documents
-        
-        secrets_manager_policy = iam_client.create_policy(
-            PolicyName=sm_policy_name,
-            PolicyDocument=json.dumps(secrets_manager_policy_document),
-            Description='Policy for accessing secret manager',
-        )
-
-        # fetch arn of this policy
-        sm_policy_arn = secrets_manager_policy["Policy"]["Arn"]
-
-        # attach policy to Amazon Bedrock execution role
-        iam_client.attach_role_policy(
-            RoleName=bedrock_kb_execution_role["Role"]["RoleName"],
-            PolicyArn=sm_policy_arn
-        )
-    
-    return bedrock_kb_execution_role
